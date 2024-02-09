1f0a23e68951d84386b15ed4605fb2c78e832f0f
Rob van Dijk <rob@optimust.nl>
2024-02-09 11:28:02 +0100

    295. Add Entity, Model, and Mapstruct

---
+++ src/main/java/guru/springframework/reactivemongo/mappers/BeerMapper.java
@@ -0,0 +1,14 @@
+package guru.springframework.reactivemongo.mappers;
+
+import guru.springframework.reactivemongo.domain.Beer;
+import guru.springframework.reactivemongo.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDTO beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDTO beerDTO);
+
+}
