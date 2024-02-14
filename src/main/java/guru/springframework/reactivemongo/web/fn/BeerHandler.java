60ac5ef960d1bad9d2e33a491d7dd5b4aafa2f8b
Rob van Dijk <rob@optimust.nl>
2024-02-14 21:27:10 +0100

    306. WebFlux Handler

---
+++ src/main/java/guru/springframework/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,22 @@
+package guru.springframework.reactivemongo.web.fn;
+
+import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+
+}
